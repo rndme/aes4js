e1ffa3538cd9fc998b69426dc160746f4b2bc79e
dandavis <rndme@users.noreply.github.com>
2017-10-14 21:04:06 -0500

    minor format and commenting

--- aes4js.js
+++ aes4js.js
@@ -5,11 +5,11 @@ function aesEnc(key, str) {
 		encoder = new TextEncoder('utf-8'),
 		encodedString = encoder.encode(str),
 		bin = false;
-	if(typeof str === "object") {
+	if(typeof str === "object") { // allows binary as well as string input
 		encodedString = str; // arrayish
 		bin = true;
 	}
-
+	
 	return derive(key).then(function(key) {
 		return window.crypto.subtle.encrypt({
 			name: "AES-GCM",
@@ -18,14 +18,14 @@ function aesEnc(key, str) {
 		}, key, encodedString)
 		  .then(function(encrypted) {
 			return window.crypto.subtle.exportKey("jwk", key)
-			  .then(function(c) {
+			  .then(function(c) { // turn bin array into exportable dataURL:
 				return new Promise(function(resolve, reject) {
 					var fr = new FileReader;
 					fr.onload = function() {
 						resolve({
-							encrypted: fr.result,
-							iv: [].slice.call(iv),
-							bin: bin
+							encrypted: fr.result, // ct
+							iv: [].slice.call(iv),// iv
+							bin: bin              // type flag
 						});
 					};
 					fr.onerror = reject;
@@ -34,27 +34,26 @@ function aesEnc(key, str) {
 			}); // end export
 		}); //end encrypt
 	}) //end derive
-	.
-	catch(console.error);
+	.catch(console.error);
 } /* end aesEnc() */
 
 	function aesDec(key, obj) {
 	if(typeof obj === "string") obj = JSON.parse(obj);
 	return derive(key).then(function(key) {
-		return new Promise(function(resolve, reject) {
+		return new Promise(function(resolve, reject) { // turn dataURL into bin array:
 			var blob = dataUrlToBlob(obj.encrypted),
 				fr = new FileReader();
-			fr.onload = function() {
+			fr.onload = function() { // feed bin array to native decrypt:
 				window.crypto.subtle.decrypt({
 					name: "AES-GCM",
 					iv: new Uint8Array(obj.iv),
 					tagLength: 128,
 				}, key, fr.result)
-				  .then(function(x){
+				  .then(function(x){ // if not bin input, decode:
 					return obj.bin ? x : new TextDecoder("utf-8").decode(x);
 				  })
 				  .then(resolve)
-				  .catch(function(y) {
+				  .catch(function(y) { // given a op err here, a wrong pw was given
 					if(String(y) === "OperationError") y = "Opps!\r\n\r\nWrong Password, try again.";
 					reject(y);
 					//resolve(y);
@@ -68,14 +67,14 @@ function aesEnc(key, str) {
 
 function sha256(str) {
 	return crypto.subtle.digest("SHA-256", new TextEncoder("utf-8").encode(str))
-	  .then(function(x) {
-		return Array.from(new Uint8Array(x)).map(function(b) {
+	  .then(function(x) {// turn into hex string:
+		return Array.from(new Uint8Array(x)).map(function(b) { 
 			return('00' + b.toString(16)).slice(-2);
 		}).join('');
 	});
 } /* end sha256() */
 
-function derive(plainText) {
+function derive(plainText) { // key derivation using 100k pbkdf w/sha256
 	if(plainText.length < 10) plainText = plainText.repeat(12 - plainText.length);
 	return sha256("349d" + plainText + "9d3458694307" + plainText.length)
 	  .then(function(salt) {
@@ -99,7 +98,7 @@ function derive(plainText) {
 } /* end derive() */
 
 
-function dataUrlToBlob(strUrl) {
+function dataUrlToBlob(strUrl) { // support util for converting bin arrays
 	var parts = strUrl.split(/[:;,]/),
 		type = parts[1],
 		decoder = parts[2] == "base64" ? atob : decodeURIComponent,
@@ -107,8 +106,6 @@ function dataUrlToBlob(strUrl) {
 		mx = binData.length,
 		i = 0,
 		uiArr = new Uint8Array(mx);
-
 	for(i; i < mx; ++i) uiArr[i] = binData.charCodeAt(i);
-
 	return new Blob([uiArr], {type: type});
 } /* end dataUrlToBlob() */
